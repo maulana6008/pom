8d01aeb26221359cf9e1d52be8c178708481d5d5
maulana6008 <adamherry123456@gmail.com>
2022-06-24 15:20:54 +0700

    Algoritm Delete Refuel

--- delete.php
+++ delete.php
@@ -34,4 +34,30 @@
                 echo "<script>location='user.php'</script>";
             }
         }
+        elseif($_GET['delete'] == "tra-del"){
+            $select = $koneksi->query("SELECT users.saldo, bensin.isi, transaksi.id_user, transaksi.id_bensin,
+            transaksi.pengisian, transaksi.total_harga from transaksi,users,bensin
+            WHERE transaksi.id_transaksi='$id' and users.id_users=transaksi.id_user
+            and bensin.id_bensin = transaksi.id_bensin ");
+            $obj = $select->fetch_object();
+            $saldo = $obj->saldo + $obj->total_harga;
+            $isi = $obj->isi + $obj->pengisian;
+            $id_user = $obj->id_user;
+            $id_bensin = $obj->id_bensin;
+            $delete = $koneksi->query("DELETE FROM transaksi WHERE id_transaksi='$id'");
+            if($delete){
+                $update = $koneksi->query("UPDATE users SET saldo='$saldo' WHERE id_users='$id_user'");
+                $update1 = $koneksi->query("UPDATE bensin SET isi='$isi' WHERE id_bensin='$id_bensin'");
+                if($update and $update1){
+                    echo "<script>alert('delete successfuly')</script>";
+                    echo "<script>location='refuel.php'</script>";
+                }else{
+                    echo "<script>alert('delete failed')</script>";
+                    echo "<script>location='refuel.php'</script>";
+                }
+            }else{
+                echo "<script>alert('delete failed')</script>";
+                echo "<script>location='refuel.php'</script>";
+            }
+        }
     }
